9f5e165f8736b3acbdd32c9e77dcbbc5f608d417
Theo Foucher <theonicolas.foucher@gmail.com>
2024-02-06 15:36:12 +0100

    formulaire fonctionel

--- Home.php
+++ Home.php
@@ -32,19 +32,23 @@
             <input type="text" name="tel" id="tel" placeholder="0669696969" required />
         <label for="adresse">Adresse : </label>
             <input type="text" name="adresse" id="adresse" placeholder="69 rue du couscous" required />
+        <label for="ville">Ville : </label>
+            <input type="text" name="ville" id="ville" placeholder="arabie city" required />
         <label for="code-postal">Code postal : </label>
             <input type="text" name="code-postal" id="code-postal" placeholder="69000" required />
         <label for="projet">Projet : </label>
             <input type="text" name="projet" id="projet" placholder="Le projet de Mohamed" required />
-        <select name="pre-inscrit" id="pre-inscrit">
-            <option value="">pré-inscrit</option>
-            <option value="oui">Oui</option>
-            <option value="non">Non</option>
+        <select name="pre_inscrit" id="pre_inscrit" required>
+            <option value="1">Oui</option>
+            <option value="0">Non</option>
         </select>
-        <select name="niveau_etude" id="niveau-etude">
-            <option value="">Niveau d'étude</option>
-            <option value=""></option>
-            <option value=""></option>
+        <select name="niveau_etude" id="niveau_etude" required>
+            <option value="3">BAC</option>
+            <option value="2">Licence</option>
+            <option value="1">Master</option>
+            <option value="4">Bac +2</option>
+            <option value="5">CAP</option>
+        </select>
         <input type="submit" name="soumettre" value="enregistrer" />
     </form>
     <?php
@@ -57,11 +61,15 @@
         $mail = $_POST['email'];
         $tel = $_POST['tel'];
         $adresse = $_POST['adresse'];
+        $ville = $_POST['ville'];
         $code_postal = $_POST['code-postal'];
+        $projet = $_POST['projet'];
+        $pre_inscrit = $_POST['pre_inscrit'];
+        $niveau_etude = $_POST['niveau_etude'];
 
         //on ajoute les valeurs dans la db
-        $sql = "INSERT INTO prospects (id, prenom, nom, email, n°_de_telephone, adresse, ville, code_postal, projet, pre_inscrit, niveau_etude, heure_enregistrement) 
-                VALUES ('$prenom', '$nom', '$email', '$tel', '$adresse', '$code_postal')";
+        $sql = "INSERT INTO prospect (prenom, nom, email, tel, adresse, ville, code_postal, projet, pre_inscrit, niveau_etude, heure_enregistrement) 
+                VALUES ('$prenom', '$nom', '$mail', '$tel', '$adresse', '$ville', '$code_postal', '$projet', '$pre_inscrit', '$niveau_etude', NOW())";
         $pdo->exec($sql);
         }
     ?>
