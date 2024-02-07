b240c98188bc0b85bb44c490baca38e2e149cef2
Theo Foucher <theonicolas.foucher@gmail.com>
2024-02-07 12:10:17 +0100

    debut des modifs sur connexion pour utiliser la bdd

--- connexion.php
+++ connexion.php
@@ -1,15 +1,21 @@
 <?php
+
+include_once("base_donnee.php");
+
 session_start();
 
-$user = "admin";
-$password = "1234";
+$user = 'SELECT nom_utilisateur FROM admin';
+$password = 'SELECT mot_de_passe FROM admin';
 $failed_to_log = "";
+
+$user_get=$pdo->query($user);
+$password_get=$pdo->query($password);
 
 if (isset($_POST['soumettre'])) {
     $user_verif = $_POST['utilisateur'];
     $password_verif = $_POST['password'];
 
-    if ($user_verif == $user and $password_verif == $password) {
+    if ($user_verif == $user_get and $password_verif == $password_get) {
         // Stocker les informations de connexion dans la session
         $_SESSION['utilisateur'] = $user_verif;
         $_SESSION['connected'] = true;
