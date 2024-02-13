a46dbd4af66ba896e674ae67dd93d1f48fa073cb
AryaMerlet <arya.merlet@gmail.com>
2024-02-13 09:47:13 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -10,6 +10,23 @@ if (!isset($_SESSION['connected']) || $_SESSION['connected'] !== true) {
 
 require_once('base_donnee.php')
 ?>
+<script>
+        function showUser(str) {
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET","admin.php?q="+str,true);
+    xmlhttp.send();
+  }
+}
+</script>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +37,19 @@ require_once('base_donnee.php')
     <title>Administrateur</title>
 </head>
 <body>
+
+<form>
+<select name="users" onchange="showUser(this.value)">
+  <option value="">Select a person:</option>
+  <option value="1">Peter Griffin</option>
+  <option value="2">Lois Griffin</option>
+  <option value="3">Joseph Swanson</option>
+  <option value="4">Glenn Quagmire</option>
+  </select>
+</form>
+<br>
+<div id="txtHint"><b>Person info will be listed here...</b></div>
+
 
     <?php
     // appeler toute les lignes de la tables prospect
