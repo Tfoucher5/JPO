7d8a555afd9003c25360513ca265a78e71014d80
Gabriel Delechamp <gabriel.delechamp.pro@gmail.com>
2024-02-16 11:24:48 +0100

    ajout titres a toutes les pages + commentaires reglages

--- Home.php
+++ Home.php
@@ -72,7 +72,7 @@ if (isset($_POST['soumettre'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;700&display=swap" rel="stylesheet">
