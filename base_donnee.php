602e589c82c3882d33cc310ea3cb3898ffe6d091
AryaMerlet <arya.merlet@gmail.com>
2024-03-25 10:33:47 +0100

    Update base_donnee.php

--- base_donnee.php
+++ base_donnee.php
@@ -1,14 +1,28 @@
 <?php
 
-    //script de connexion
-    $host = '127.0.0.1';
-    $db = 'jpo';
-    $user = 'root';
-    $pass = '';
-    $port = '3306';
-    $charset = 'utf8mb4';
+class Database{
+    public static function getPDO(){
+        $host = '127.0.0.1';
+        $db = 'JPO';
+        $user = 'root';
+        $pass = '';
+        $port = '3306';
+        $charset = 'utf8mb4';
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+        $pdo = new PDO($dsn, $user, $pass);
+        return $pdo;
+    }
+    public static function preparedQuery($sql, $params = []) {
+        $pdo = self::getPDO();
+        $stmt = $pdo->prepare($sql);
+        
+        foreach ($params as $param => $value) {
+            $stmt->bindParam($param, $value);
+        }
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+ 
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
-    $pdo = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
-    $pdo->exec("SET CHARACTER SET utf8mb4");
+}
